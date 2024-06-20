f5127777510d40fb25e62eed0cf8c9731d8c051f
Mikhail Osadchiy <lava1804.92@gmail.com>
2024-06-19 21:25:02 -0400

    added quick_sort.py

---
+++ quick_sort.py
@@ -0,0 +1,11 @@
+def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+
+print(quick_sort([5, 3, 6, 2, 10]))
